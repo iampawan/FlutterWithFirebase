4b94299494b4e96f2e8e04e789cc72c05c1c4cd5
Pawan Kumar <pawan221b@gmail.com>
2018-04-03 14:48:41 +0530

    Added Cloud Firestore - With All CRUD Operation Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -20,7 +20,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomePageState createState() {
+    return new MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  String myText = null;
+  StreamSubscription<DocumentSnapshot> subscription;
+
+  final DocumentReference documentReference =
+      Firestore.instance.document("myData/dummy");
+
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn googleSignIn = new GoogleSignIn();
 
@@ -38,6 +51,63 @@ class MyHomePage extends StatelessWidget {
   void _signOut() {
     googleSignIn.signOut();
     print("User Signed out");
+  }
+
+  void _add() {
+    Map<String, String> data = <String, String>{
+      "name": "Pawan Kumar",
+      "desc": "Flutter Developer"
+    };
+    documentReference.setData(data).whenComplete(() {
+      print("Document Added");
+    }).catchError((e) => print(e));
+  }
+
+  void _delete() {
+    documentReference.delete().whenComplete(() {
+      print("Deleted Successfully");
+      setState(() {});
+    }).catchError((e) => print(e));
+  }
+
+  void _update() {
+    Map<String, String> data = <String, String>{
+      "name": "Pawan Kumar Updated",
+      "desc": "Flutter Developer Updated"
+    };
+    documentReference.updateData(data).whenComplete(() {
+      print("Document Updated");
+    }).catchError((e) => print(e));
+  }
+
+  void _fetch() {
+    documentReference.get().then((datasnapshot) {
+      if (datasnapshot.exists) {
+        setState(() {
+          myText = datasnapshot.data['desc'];
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    subscription = documentReference.snapshots.listen((datasnapshot) {
+      if (datasnapshot.exists) {
+        setState(() {
+          myText = datasnapshot.data['desc'];
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    subscription?.cancel();
   }
 
   @override
@@ -67,6 +137,47 @@ class MyHomePage extends StatelessWidget {
               child: new Text("Sign out"),
               color: Colors.red,
             ),
+            new Padding(
+              padding: const EdgeInsets.all(10.0),
+            ),
+            new RaisedButton(
+              onPressed: _add,
+              child: new Text("Add"),
+              color: Colors.cyan,
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(10.0),
+            ),
+            new RaisedButton(
+              onPressed: _update,
+              child: new Text("Update"),
+              color: Colors.lightBlue,
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(10.0),
+            ),
+            new RaisedButton(
+              onPressed: _delete,
+              child: new Text("Delete"),
+              color: Colors.orange,
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(10.0),
+            ),
+            new RaisedButton(
+              onPressed: _fetch,
+              child: new Text("Fetch"),
+              color: Colors.lime,
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(10.0),
+            ),
+            myText == null
+                ? new Container()
+                : new Text(
+                    myText,
+                    style: new TextStyle(fontSize: 20.0),
+                  )
           ],
         ),
       ),
